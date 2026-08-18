b97374b0847eeb7bea412ae5b81c17c7510a8c1f
Aravind <26415600+asura6194@users.noreply.github.com>
2026-08-18 17:50:09 +0530

    deployment change

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/aravind-portfolio/" : "/",
+export default defineConfig({
+  base: "/",
   server: {
     port: 5173,
   },
-}));
+});
