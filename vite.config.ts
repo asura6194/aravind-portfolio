843682c4c865f0ab13c30d5119b1676e67b24f7f
Aravind <26415600+asura6194@users.noreply.github.com>
2026-08-18 18:19:03 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  base: "./",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/aravind-portfolio/" : "/",
   server: {
     port: 5173,
   },
-});
+}));
